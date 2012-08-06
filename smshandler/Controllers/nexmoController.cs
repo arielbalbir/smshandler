49798a3ec001e49aed16bea622de910e0c3d91ff
John Campbell, Jr <johncampbell@gmail.com>
2012-08-06 14:40:49 -0400

    Addes newtonsoft.json, use regular controller.

--- smshandler/Controllers/nexmoController.cs
+++ smshandler/Controllers/nexmoController.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Web.Http;
+﻿using System.Web.Mvc;
 
 namespace smshandler.Controllers
 {
     /// <summary>
     /// Called via the route api/nexmo
+    /// 
+    /// You can find the nexmo documentation here: http://nexmo.com/documentation/index.html
+    /// Handling the inbound message: http://nexmo.com/documentation/index.html#mo
+    /// Creating the outgoing text message: http://nexmo.com/documentation/index.html#txt
     /// </summary>
-    public class NexmoController : ApiController
+    public class NexmoController : Controller
     {
         public static string commandFile = "nexmoCommandFile.txt";
         // GET api/values
@@ -14,13 +17,29 @@ namespace smshandler.Controllers
         {
             // Log Text Message
             // Read the command file and Create Outgoing Text Message
-            CreateOutgoingMessage("message");
+            SendOutgoingMessage("message");
             return "Success";
         }
 
-        private void CreateOutgoingMessage(string message)
+        private void SendOutgoingMessage(string message)
         {
-            
+            var json = CreateOutgoingMessage(message);
+            // make an http request to nexmo here.
+        }
+
+        private string CreateOutgoingMessage(string message)
+        {
+            return message;  // should return the json string as specified in the nexmo documentation
+        }
+
+        /// <summary>
+        /// Use this method as an easy way to test the outgoing sms message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public string GetMessageJson(string message)
+        {
+            return CreateOutgoingMessage(message);
         }
     }
 }
