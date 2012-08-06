2e72b3f21dab8c1f53d20787718bf156045a0e2e
unknown <Administrator@BASEGUIDEWEBAPP.(none)>
2012-08-06 14:00:18 -0400

    Inital commit.

---
+++ smshandler/Controllers/nexmoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace smshandler.Controllers
+{
+    /// <summary>
+    /// Called via the route api/nexmo
+    /// </summary>
+    public class NexmoController : ApiController
+    {
+        public static string commandFile = "nexmoCommandFile.txt";
+        // GET api/values
+        public string Get()
+        {
+            // Log Text Message
+            // Read the command file and Create Outgoing Text Message
+            CreateOutgoingMessage("message");
+            return "Success";
+        }
+
+        private void CreateOutgoingMessage(string message)
+        {
+            
+        }
+    }
+}
