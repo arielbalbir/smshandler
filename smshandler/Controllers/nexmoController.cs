6d80f60517596b6eb556f87c6cb7db1e4a7d8a1d
arielbalbir <ariel.balbir@gmail.com>
2012-08-13 03:19:12 -0700

    Update smshandler/Controllers/nexmoController.cs
    Changes in nexmoController

--- smshandler/Controllers/nexmoController.cs
+++ smshandler/Controllers/nexmoController.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Mvc;
-
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using System;
+using log4net;
+using log4net.Config;
 namespace smshandler.Controllers
 {
     /// <summary>
@@ -11,35 +15,62 @@ namespace smshandler.Controllers
     /// </summary>
     public class NexmoController : Controller
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static string commandFile = "nexmoCommandFile.txt";
-        // GET api/values
+        HttpWebRequest objWebRequest = null;
+        //Set uesrname
+        String username = "";
+        //Set password
+        String password = "";
+        //Set from phone number
+        String fromPhone = "MyCompany20";
+        //Set to phone number
+        String ToPhone = "";
+        // GET api/values 
         public string ProcessText()
         {
             // Log Text Message
-            // Read the command file and Create Outgoing Text Message
-            SendOutgoingMessage("message");
+            log.Debug("Application started");
+            // Read the command file and Create Outgoing Text Message 
+            string fileName = Server.MapPath("~") + @"\" + commandFile;
+            String message = "";
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(fileName))
+            {
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                 message = message + line;
+                }
+            }
+            log.Info("to number:" + ToPhone + ", from number:" + fromPhone + ", messageId:, message timestamp:, text:"+message+"");
+            SendOutgoingMessage(message);
             return "Success";
         }
-
         private void SendOutgoingMessage(string message)
         {
+        
+            objWebRequest = (HttpWebRequest)WebRequest.Create("http://rest.nexmo.com/sms/json?username=" + username + "&password=" + password + "&from=" + fromPhone + "&to=" + ToPhone + "&text=" + message );
+            objWebRequest.Method = "POST";
             var json = GenerateMessageUrl(message);
-            // make an http request to nexmo here.
+           // make an http request to nexmo here. 
         }
-
-        private string GenerateMessageUrl(string message)
+        private  string GenerateMessageUrl(string message)
         {
-            return message;  // should return the json string as specified in the nexmo documentation
+            HttpWebResponse response = (HttpWebResponse)objWebRequest.GetResponse();
+            Stream streamResponse = response.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResponse);
+            String responsestring = streamRead.ReadToEnd();
+            return responsestring; //should return the json string as specified in the nexmo documentation
         }
 
         /// <summary>
-        /// Use this method as an easy way to test the outgoing sms message
+        /// Use this method as an easy way to test the outgoing sms message   https://github.com/arielbalbir/smshandler
         /// </summary>
         /// <param name="message"></param>
         /// <returns></returns>
         public string GetMessageUrl(string message)
         {
-            return GenerateMessageUrl(message);
+           return GenerateMessageUrl(message);
         }
     }
 }
